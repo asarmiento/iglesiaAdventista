a949396ffc1a0681d88ec0d456085dc983b6b79c
Francisco Gamonal <francisco741@hotmail.com>
2015-01-06 09:36:16 -0500

    configuración entornos de desarrollo

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'gbook_iglesiaquepos',
-			'username'  => 'root',
-			'password'  => 'F4cc0unt',
+			'database'  => 'forge',
+			'username'  => 'forge',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
