07a1cc004831f2e6dbe105da3146be414ef49537
Francisco Gamonal <francisco741@hotmail.com>
2015-01-22 14:01:15 -0500

    fix ajaxform

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,32 @@
 var ajaxForm = function (resource, type, data){
 	var server = 'http://localhost/iglesiaQuepos/public/';
 	console.log(type, resource, data);
-	$.ajax({
-		url: server + resource + '/' +data,
-        type: type,
-        data: data,
-        beforeSend: function(){
-            console.log('Registrando');
-        },
-        error:function(){
-            console.log("No se pueden grabar los datos.");
-        }
-	});
+	return $.ajax({
+			url: server + resource + '/' +data.id,
+		    type: type,
+		    data: {data: data},
+		    datatype: 'json',
+		    beforeSend: function(){
+		        console.log('Registrando');
+		    },
+		    error:function(){
+		        console.log("No se pueden grabar los datos.");
+		    }
+		});
 };
+
+var data = {};
 
 $(function(){
 	$(document).on("click", "#btnRemoveTypeUser", function(e){
 		e.preventDefault();
 		var resource = $(this).data("resource");
-		var data = $(this).parent().parent().find('.user_number').text();
-		ajaxForm(resource, "delete", data);
+		var id = $(this).parent().parent().find('.user_number').text();
+		data.id = id;
+		console.log(data)
+		ajaxForm(resource, "delete", data)
+		.done(function(result){
+			console.log(result);
+		});
 	});
 });
