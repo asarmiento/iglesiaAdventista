9a136281df9294e8a9f434de14eb6944088e9a2b
Anwar Sarmiento <anwarsarmiento@gmail.com>
2016-03-03 20:30:08 -0600

    cambio en saldo de deposito

--- app/Http/Routes/routes_banks.php
+++ app/Http/Routes/routes_banks.php
@@ -7,7 +7,14 @@
  */
 Route::get('bancos/ver', ['as' => 'bank-ver', 'uses' => 'BankController@index']);
 Route::get('bancos/create', ['as' => 'bank-create', 'uses' => 'BankController@create']);
+Route::post('bancos/create', ['as' => 'bank-store', 'uses' => 'BankController@store']);
+
+
 Route::get('bancos/depositos/ver', ['as' => 'deposito-ver', 'uses' => 'BankController@deposit']);
 Route::get('bancos/depositos/create', ['as' => 'create-deposit', 'uses' => 'BankController@depositCreate']);
 Route::post('bancos/depositos/create', ['as' => 'post-deposit', 'uses' => 'BankController@depositStore']);
-Route::post('bancos/create', ['as' => 'bank-store', 'uses' => 'BankController@store']);
+
+
+Route::get('bancos/depositos-campo-loca/ver', ['as' => 'deposito-campo-ver', 'uses' => 'BankController@depositCampo']);
+Route::get('bancos/depositos-campo-loca/create', ['as' => 'create-deposit-campo', 'uses' => 'BankController@depositCampoCreate']);
+Route::post('bancos/depositos-campo-loca/create', ['as' => 'store-deposit-campo', 'uses' => 'BankController@depositCampoStore']);
