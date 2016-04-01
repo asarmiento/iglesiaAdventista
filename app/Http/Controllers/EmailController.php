65644596b75111f3c778a1cd0bc5340f0bcb6e17
Anwar Sarmiento <anwarsarmiento@gmail.com>
2016-03-31 19:55:31 -0600

    prueba de email

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace SistemasAmigables\Http\Controllers;
+
+use Illuminate\Http\Request;
+use SistemasAmigables\Http\Requests;
+use Mail;
+
+class EmailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data =array('email' => 'asarmiento@sistemasamigables.com');
+        $message='Prueba de Correo';
+        Mail::send('emails.welcome', $data, function ($message) {
+
+            $message->from('anwarsarmiento@gmail.com', 'Laravel');
+
+            $message->to('asarmiento@sistemasamigables.com')->cc('info@sistemasamigables.com');
+        });
+
+        print('Enviado');
+    }
+
+    public function sendEmailReminder(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        Mail::send('emails.reminder', ['user' => $user], function ($m) use ($user) {
+            $m->from('hello@app.com', 'Your Application');
+
+            $m->to($user->email, $user->name)->subject('Your Reminder!');
+        });
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,26 @@
+<!--
+ * Created by PhpStorm.
+ * User: anwar
+ * Date: 31/03/16
+ * Time: 07:24 PM
+ */-->
+
+
+Nombre: Rafael Torrealba<br>
+Asunto: Email Preview<br>
+Telf: 123456<br>
+
+
+<p>Texto de prueba para el email preview.</p>
+1
+2
+3
+4
+5
+6
+Nombre: Rafael Torrealba<br>
+Asunto: Email Preview<br>
+Telf: 123456<br>
+
+
+<p>Texto de prueba para el email preview.</p>
